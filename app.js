10ca731f33264218c1549d09ba557026a9c35b30
Philip Powers <philip_powers607@hotmail.com>
2021-09-14 13:30:14 -0400

    user input captured

---
+++ app.js
@@ -0,0 +1,21 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+
+
+
+const printProfilData = profileDataArr => {
+   for (let i = 0; i < profileDataArr.length; i += 1) {
+     console.log(profileDataArr[i]);  
+   } 
+
+
+   console.log('================');
+
+   profileDataArr.forEach(profileItem => console.log(profileItem));
+   };
+
+
+printProfilData(profileDataArgs);
