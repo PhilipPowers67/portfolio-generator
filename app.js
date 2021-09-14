44f603f6a57d8c398a02f4648b7376f1a8011bb6
Philip Powers <philip_powers607@hotmail.com>
2021-09-14 16:20:31 -0400

    HTML page created

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
 
 
 
 
-const printProfilData = profileDataArr => {
-   for (let i = 0; i < profileDataArr.length; i += 1) {
-     console.log(profileDataArr[i]);  
-   } 
 
 
-   console.log('================');
-
-   profileDataArr.forEach(profileItem => console.log(profileItem));
-   };
 
 
-printProfilData(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var notNeeded = function() {
+// // const printProfileData = profileDataArr => {
+// //    for (let i = 0; i < profileDataArr.length; i += 1) {
+// //      console.log(profileDataArr[i]);  
+// //    } 
+
+
+// //    console.log('================');
+
+// //    profileDataArr.forEach(profileItem => console.log(profileItem));
+// //    };
+
+
+// // printProfileData(profileDataArgs);
+// };
